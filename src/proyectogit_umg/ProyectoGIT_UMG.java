6427ea0de85f6b5e9136c8ce7a683585f484a09b
90NR0JE7M002D0 <90NR0JE7M002D0@LAPTOP-C7SM1R4H>
2026-04-15 12:32:23 -0600

    Primera version del proyecto

---
+++ src/proyectogit_umg/ProyectoGIT_UMG.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectogit_umg;
+
+/**
+ *
+ * @author 90NR0JE7M002D0
+ */
+public class ProyectoGIT_UMG {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo!");
+       
+    }
+    
+}
